63cc5588551f270037b29241360d325f7ab25027
Louis Joseph Bishop <louis.bishop@live.co.uk>
2020-12-04 12:07:25 +0000

    oop

--- RouteFinder/src/routefinder/DjikstraOperations.java
+++ RouteFinder/src/routefinder/DjikstraOperations.java
@@ -5,10 +5,30 @@
  */
 package routefinder;
 
+import java.util.Scanner;
+
 /**
  *
  * @author l-bishop
  */
-public class DjikstraOperations {
-    
+public class DjikstraOperations 
+{
+    public void menu()
+    {
+        System.out.println("Here you can calculate various routes between two points using a variety of conditions.");
+        Scanner scanner = new Scanner(System.in);
+        int userSelection;
+        do
+        {
+            System.out.println("Please enter a valid menu option to proceed.\n");
+            System.out.println("1. Calculate Shortest Route\n2. Calculate Straightest Route\n3. Calculate Quietest Route\n4. Calculate Quietest+Straightest Route\n5. Exit");
+            while (!scanner.hasNextInt()) 
+            {
+                System.out.println("That's not a number!");
+                scanner.next();
+            }
+            userSelection = scanner.nextInt();
+        }
+        while (!(userSelection >= 1 && userSelection <=3));
+    }
 }
